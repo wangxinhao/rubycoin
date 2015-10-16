8e9eb7253423371934440c918fa1ad86bd36be23
FuzzyHobbit <fuzzyhobbit@gmx.com>
2015-10-16 01:00:17 -0400

    Fix URI parsing

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -129,13 +129,9 @@ bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
 
 bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
 {
-    // Convert rubycoin:// to rubycoin:
-    //
-    //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
-    //    which will lower-case it (and thus invalidate the address).
     if(uri.startsWith("rubycoin://"))
     {
-        uri.replace(0, 12, "rubycoin:");
+        uri.replace(0, 11, "rubycoin:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
