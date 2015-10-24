a66a32f6f907a638738c2c59939b9d3b72b42cd1
FuzzyHobbit <fuzzyhobbit@gmx.com>
2015-10-24 05:09:28 -0400

    Fixed client versioning

--- src/version.h
+++ src/version.h
@@ -13,9 +13,9 @@
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
-                         +   00000 * CLIENT_VERSION_MINOR
-                         +     000 * CLIENT_VERSION_REVISION
-                         +       0 * CLIENT_VERSION_BUILD;
+                         +   10000 * CLIENT_VERSION_MINOR
+                         +     100 * CLIENT_VERSION_REVISION
+                         +       1 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
@@ -24,6 +24,7 @@ extern const std::string CLIENT_DATE;
 //
 // database format versioning
 //
+
 static const int DATABASE_VERSION = 70000;
 
 //
