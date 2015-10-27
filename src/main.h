82a25917277d574caec1f708b67cced932596e2b
FuzzyHobbit <fuzzyhobbit@gmx.com>
2015-10-27 10:13:31 -0400

    Add switch for protocol v3

--- src/main.h
+++ src/main.h
@@ -54,6 +54,7 @@ static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20
 static const int64_t COIN_YEAR_REWARD = 5 * CENT; // 5% per year
 
 inline bool IsProtocolV2(int nHeight) { return nHeight > 7000; }
+inline bool IsProtocolV3(int64_t nTime) { return TestNet() || nTime > 1446879600; }
 
 inline int64_t FutureDriftV1(int64_t nTime) { return nTime + 10 * 60; }
 inline int64_t FutureDriftV2(int64_t nTime) { return nTime + 15; }
